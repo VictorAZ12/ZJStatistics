ebdf095d2454947f9a636176048b7f29d42e0e4d
YanchenZhao233 <819413991zhao@gmail.com>
2023-03-28 00:35:39 +0800

    Extracted calendar from 2 html files

--- calendar/README.md
+++ calendar/README.md
@@ -4,7 +4,7 @@
 对B站@A-SOUL_Offical已爬取的动态进行人工筛选，挑选出所有日程表相关的动态进行查看，存入Notion表格内
 ### 直播 Stream
 由于技术不精，使用了一种朴素的方法：将[奶粉老师的录播站](https://alist.ddindexs.com/)上一周年、二周年的文件夹页面保存到本地，用正则从html文件里提取每个视频的名字来获取直播的日期和标题，最后结合直播预告动态进行人工校对。
-### 时间戳转换 Time stamp conversion
+
 
 ## 可视化 Visualisation
 使用Notion建立日程表的calendar view。
---
+++ calendar/stream_time_extract.py
@@ -0,0 +1,28 @@
+import re
+import csv
+import os
+years = []
+# Read in HTML files of 2 folder pages
+# See https://alist.ddindexs.com/
+with open("./calendar/anni1.html", "r", encoding="UTF-8") as f:
+    years.append(f.read())
+with open("./calendar/anni2.html", "r", encoding="UTF-8") as f:
+    years.append(f.read())
+# initialise pattern and output
+titles = []
+pattern = r"""title=".*?\.mp4\"*"""
+
+row_list = []
+# find all titles and extract rows
+for year in years:
+    for i, title in enumerate(re.findall(pattern, year)):
+        row = [title[18:-5], title[7:17].replace('.','/')]
+        print(row)
+        row_list.append(row)
+
+with open("./calendar/calendar.csv", 'w', encoding="UTF-8", newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    # Column names
+    writer.writerow(["Title", "Date"])
+    for row in row_list:
+        writer.writerow(row)
